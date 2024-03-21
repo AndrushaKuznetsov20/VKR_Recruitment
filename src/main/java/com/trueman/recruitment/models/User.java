33030b5df27d8773333c3ab0973b2189166cd230
Kuznetsov20 <andrusha02.2002@mail.ru>
2024-03-21 21:38:58 +0300

    Добавление Spring_security

---
+++ src/main/java/com/trueman/recruitment/models/User.java
@@ -0,0 +1,62 @@
+package com.trueman.recruitment.models;
+
+import com.trueman.recruitment.models.enums.Role;
+import jakarta.persistence.*;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "username", unique = true, nullable = false)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "email", unique = true, nullable = false)
+    private String email;
+
+    @Column(name = "active")
+    private boolean active;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false)
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return active;
+    }
+}
---
+++ src/main/java/com/trueman/recruitment/models/enums/Role.java
@@ -0,0 +1,7 @@
+package com.trueman.recruitment.models.enums;
+
+public enum Role {
+    ROLE_USER,
+    ROLE_MODER,
+    ROLE_ADMIN
+}
